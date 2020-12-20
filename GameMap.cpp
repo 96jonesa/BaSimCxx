f827fc8c08717daf2675b1360cf42d998206dac8
96jonesa <ajones96@uw.edu>
2020-12-20 06:08:44 -0800

    more fixes

--- GameMap.cpp
+++ GameMap.cpp
@@ -27,7 +27,7 @@ GameMap::GameMap(bool is_wave_10) : is_wave_10_(is_wave_10) {
 
   for (int x_zone = 0; x_zone < item_zones_width_; x_zone++) {
     for (int y_zone = 0; y_zone < item_zones_height_; y_zone++) {
-     item_zones_[x_zone + item_zones_width_ * y_zone] = new std::vector< std::shared_ptr<Item> >();
+     item_zones_[x_zone + item_zones_width_ * y_zone] = std::shared_ptr< std::vector< std::shared_ptr<Item> >(new std::vector< std::shared_ptr<Item> >());
     }
   }
 
